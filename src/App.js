d93ed9b73299028693862814325f7eaaba1eaa0f
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-07-21 10:30:26 +0700

    update project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
+import MyClock from "./components/MyClock";
 
 function App() {
     return (
-        <div></div>
+        <MyClock/>
     );
 }
 
---
+++ src/components/MyClock.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import useClock from "../hooks/useClock";
+
+function MyClock() {
+    //Gọi custom hook để sử dụng
+    const [time, ampm] = useClock();
+    return (
+        <div id="Clock-style">
+            {time}
+            <span> {ampm}</span>
+        </div>
+    );
+}
+
+export default MyClock
